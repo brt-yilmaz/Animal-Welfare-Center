a9d344d1c95f8bc943c0b58e0f9ed75f6a547068
Berat Yilmaz <beratyilmaz3102@gmail.com>
2023-06-05 14:27:09 +0200

    Delete unneccessary import

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { Providers } from "./store/provider";
-import { MdPets } from "react-icons/md";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Providers>{children}</Providers>
       </body>
     </html>
